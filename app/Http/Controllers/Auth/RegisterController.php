1d20b0d54e1986c8a92e1932248a279a50d69b71
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2022-10-20 13:39:40 +0700

    Configuring auth, the route, etc. (V.1), and adding navbar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'email' => 'fajarmi521@gmail.com',
             'password' => Hash::make($data['password']),
         ]);
     }
